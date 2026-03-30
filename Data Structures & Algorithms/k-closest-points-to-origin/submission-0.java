8e9d320d6349dff0098b950b023a7eb73de7ee1f
Kavia-M <63632108+Kavia-M@users.noreply.github.com>
2026-03-30 14:13:20 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,61 @@
+// using max_heap
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            int distA = a[0]*a[0] + a[1]*a[1];
+            int distB = b[0]*b[0] + b[1]*b[1];
+
+            return Integer.compare(distA, distB); // max heap as distB comes first
+        });
+        // space O(n-k)
+        int[][] ans = new int[k][2];
+        int i = 0;
+        int n = points.length;
+        for(int[] point : points) {
+            for(int[] p :pq) {
+                System.out.println(Arrays.toString(p));
+            }
+            pq.add(point);
+            if(pq.size() > (n-k)) {
+                ans[i] = pq.peek();
+                i++;
+                pq.poll();
+            }  
+            for(int[] p :pq) {
+                System.out.println(Arrays.toString(p));
+            }
+        }
+        return ans;
+
+        // return pq.toArray(new int[pq.size()][2]); // typed array, return type is passes in () in toArray
+
+    }
+}
+
+// Min heap correct implementation
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> pq;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.pq = new PriorityQueue<>(); // default min // space O(k)
+        for(int num : nums) {
+            // add it
+            pq.offer(num); // O(log k) since we maintain k size for pq
+
+            if(pq.size() > k) {
+                // remove least elenent
+                pq.poll(); // O(log k)
+            }
+        }
+    }
+    
+    public int add(int val) { // called m times as m times add is called
+        // max_heap
+        pq.offer(val); // O(log k)
+        if(pq.size() > k) 
+            pq.poll(); // O(log k)
+        
+        return pq.peek(); // O(1)
+    }
+}
