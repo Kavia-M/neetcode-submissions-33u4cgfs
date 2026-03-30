863710580dc77780965c83d6b08d33c4341b2004
Kavia-M <63632108+Kavia-M@users.noreply.github.com>
2026-03-30 15:08:51 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int max_count = 0;
+        for(char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+        int sum = 0;
+        for(int i : map.values()) {
+            if(i > max) {
+                max = i;
+            }
+            if(i == max) {
+                
+                max_count++;
+            }
+            sum+=i;
+        }
+        System.out.println(map.toString());
+        System.out.println(max);
+        System.out.println(max_count);
+        return Math.max(sum, ((n+1)*(max-1)) + max_count);
+    }
+}
