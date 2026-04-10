b169677a4fe03b474316dda3e709f0be388fb340
Kavia-M <63632108+Kavia-M@users.noreply.github.com>
2026-04-10 22:51:42 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+        public List<String> generateParenthesis(int n) {
+                List<String> ans = new ArrayList<>();
+                        generate("", ans, 0, 0, n);
+                                return ans;
+                                    }
+                                        private void generate(String s, List<String> ans, int open, int close, int n) {
+                                                // base condition
+                                                        if(open > n) {
+                                                                    return;
+                                                                            }
+
+                                                                                    if(open+close == 2*n) {
+                                                                                                ans.add(s);
+                                                                                                            return;
+                                                                                                                    }
+
+                                                                                                                            if(open > close) 
+                                                                                                                                        generate(s+")", ans, open, close+1, n);
+                                                                                                                                                
+                                                                                                                                                        generate(s+"(", ans, ++open, close, n);
+                                                                                                                                                            }
+                                                                                                                                                            
+
+}
